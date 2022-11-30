aea05d8bfd4bc161bfa1e12655edea1f12c7b3ee
Alan Wan <alan.wan@oocl.com>
2022-11-30 14:27:01 +0800

    Feat: finished stream practices

--- src/main/java/com/thoughtworks/collection/StreamReduce.java
+++ src/main/java/com/thoughtworks/collection/StreamReduce.java
@@ -1,18 +1,27 @@
 package com.thoughtworks.collection;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamReduce {
 
     public int getLastOdd(List<Integer> numbers) {
-        return 0;
+        return numbers
+                .stream()
+                .filter(num -> num % 2 == 1)
+                .reduce((accumalator, num) -> num)
+                .get();
     }
 
     public String getLongest(List<String> words) {
-        return null;
+
+        return words
+                .stream()
+                .reduce((accumalator, word) -> accumalator.length() > word.length() ? accumalator : word).get();
+
     }
 
     public int getTotalLength(List<String> words) {
-        return 0;
+        return words.stream().mapToInt(String::length).reduce(0, Integer::sum);
     }
 }
